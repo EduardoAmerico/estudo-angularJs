69c4d138e4e5c6c3ce05eead2f58286726937ac3
Eduardo Americo <eduardoamerico_@live.com>
2021-03-16 07:41:50 -0300

    inicio cap8 /2

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -2,12 +2,28 @@ angular.module('alurapic').controller('FotosController', function ($scope, $http
 
 	$scope.fotos = [];
 	$scope.filtro = '';
+	$scope.mensagem = '';
 	$http.get('v1/fotos')
 	.success(function(fotos){
 		$scope.fotos = fotos;
 	}).error(function(error){
 		console.log(error)
 	});
+
+	$scope.remover = function(foto) {
+		$http.delete('v1/fotos/' + foto._id)
+		.success(function(){
+
+			var indiceFoto = $scope.fotos.indexOf(foto);
+			$scope.fotos.splice(indiceFoto, 1);
+
+			$scope.mensagem = ('Foto '+foto.titulo+' foi removida')
+		})
+		.error(function(erro){
+			$scope.mensagem = ('Não coseguiu remover a foto: '+ foto.titulo);
+		})
+	};
+
 
 	// $scope.fotos = [];
 	// var promise =	$http.get('v1/fotos');
