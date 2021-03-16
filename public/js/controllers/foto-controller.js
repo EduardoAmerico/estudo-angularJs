06c82e411a32402d13fb2df40a522ac2f81c34c1
Eduardo Americo <eduardoamerico_@live.com>
2021-03-16 08:18:23 -0300

    fim editar / remover cap8

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,35 +1,50 @@
 // public/js/controllers/foto-controller.js
 
 angular.module('alurapic')
-    .controller('FotoController', function($scope, $http, $routeParams) {
+    .controller('FotoController', function ($scope, $http, $routeParams) {
 
-        if($routeParams.fotoId){
-            $http.get('v1/fotos/' + $routeParams.fotoId)
-            .success(function(){
-                $scope.foto = foto;
-               
-            })
-            .error(function(erro){
-                console
-                $scope.mensagem ='Erro ao buscar foto: ' + erro;
-            });
-        }
         $scope.foto = {};
         $scope.mensagem = '';
 
-        $scope.submeter = function() {
-           if ($scope.formulario.$valid){
-            $http.post('v1/fotos', $scope.foto)
-            .success(function(){
-                $scope.foto = {};
-                $scope.mensagem ='Foto cadastrada com sucesso';
-            })
-            .error(function(erro){
-                $scope.mensagem ='Erro ao incluir formulario: ' + erro;
-            });
-           }
-            
+        if ($routeParams.fotoId) {
+            $http.get('v1/fotos/' + $routeParams.fotoId)
+                .success(function (foto) {
+                    $scope.foto = foto;
+
+                })
+                .error(function (erro) {
+                    console
+                    $scope.mensagem = 'Erro ao buscar foto: ' + erro;
+                });
+        }
+
+
+        $scope.submeter = function () {
+
+            if ($scope.foto._id) {
+                $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+                .success(function () {
+                    $scope.foto = {};
+                    $scope.mensagem = 'Foto editada com sucesso';
+                })
+                .error(function (erro) {
+                    $scope.mensagem = 'Erro ao editar a foto: ' + $scope.foto.titulo;
+                });
+            } else {
+                if ($scope.formulario.$valid) {
+                    $http.post('v1/fotos', $scope.foto)
+                        .success(function () {
+                            $scope.foto = {};
+                            $scope.mensagem = 'Foto cadastrada com sucesso';
+                        })
+                        .error(function (erro) {
+                            $scope.mensagem = 'Erro ao incluir formulario: ' + erro;
+                        });
+                }
+
+            }
+
         };
-        
+
 
     });
